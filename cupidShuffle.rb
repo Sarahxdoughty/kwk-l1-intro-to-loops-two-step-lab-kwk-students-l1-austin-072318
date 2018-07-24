f137bc1e9707c1ebdfc998d9d3a88eb86ba71a32
Sarahxdoughty <sarahxdoughty@gmail.com>
2018-07-24 18:27:32 +0000

    automatically backed up by learn

---
+++ cupidShuffle.rb
@@ -0,0 +1,13 @@
+def cupidShuffle
+  count = 0
+  loop do
+    if count == 3
+      break
+    end
+    puts "to the right"
+    puts "to the left"
+    count = count + 1
+  end
+end
+
+cupidShuffle
